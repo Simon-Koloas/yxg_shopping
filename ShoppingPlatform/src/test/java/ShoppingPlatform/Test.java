6d54714cdfab944236fc7f917737f673144b3d28
Simon-Koloas <139613204+Simon-Koloas@users.noreply.github.com>
2023-07-19 09:00:48 +0800

    Add files via upload

---
+++ ShoppingPlatform/src/test/java/ShoppingPlatform/Test.java
@@ -0,0 +1,27 @@
+package ShoppingPlatform;
+
+import ShoppingPlatform.OrderUpdate.OrderUpdate;
+import ShoppingPlatform.ProductUpdate.ProductUpdate;
+import ShoppingPlatform.TableInformation.Address;
+import ShoppingPlatform.DBUtil.DBUtil;
+import ShoppingPlatform.view.MainView;
+import sun.applet.Main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Simon
+ * @Date: 2023/07/17/19:44
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        MainView.mainView();
+    }
+}
